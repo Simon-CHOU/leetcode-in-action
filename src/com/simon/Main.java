fbadc4a57628b8302c58ac891d5d3f99c0250be2
Simon-CHOU <mrsimonzhou@gmail.com>
2021-01-29 01:28:18 +0800

    111 minimum-depth-of-binary-tree(bfs)

--- src/com/simon/Main.java
+++ src/com/simon/Main.java
@@ -2,10 +2,8 @@ package com.simon;
 
 import com.simon.util.*;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.util.LinkedList;
 
 public class Main {
     private static Solution solution = new Solution();
@@ -23,20 +21,35 @@ public class Main {
 
 
 class Solution {
+    class QueueNode {
+        TreeNode node;
+        int depth;
+        public QueueNode(TreeNode node, int depth){
+            this.node = node;
+            this.depth = depth;
+        }
+    }
     public int minDepth(TreeNode root) {
-        if (root == null) {
+        if(root == null) {
             return 0;
         }
-        if (root.left == null && root.right == null) {
-            return 1;
+
+        Queue<QueueNode> queue = new LinkedList<>();
+        queue.offer(new QueueNode(root, 1));
+        while(!queue.isEmpty()){
+            QueueNode nodeDepth = queue.poll();
+            TreeNode node = nodeDepth.node;
+            int depth = nodeDepth.depth;
+            if(node.left == null && node.right ==null){
+                return depth;
+            }
+            if( node.left != null){
+                queue.offer(new QueueNode(node.left, depth + 1));
+            }
+            if( node.right != null){
+                queue.offer(new QueueNode(node.right, depth + 1));
+            }
         }
-        int min_depth = Integer.MAX_VALUE;
-        if (root.left != null) {
-            min_depth = Math.min(minDepth(root.left), min_depth);
-        }
-        if (root.right != null) {
-            min_depth = Math.min(minDepth(root.right), min_depth);
-        }
-        return min_depth + 1;
+        return 0;
     }
 }
