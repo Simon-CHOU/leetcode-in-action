f557ad7c27de501c0228a1e648837e62e3450a80
Simon-CHOU <mrsimonzhou@gmail.com>
2021-02-04 16:42:22 +0800

    101 symmetric-tree 迭代

--- src/com/simon/Main.java
+++ src/com/simon/Main.java
@@ -2,9 +2,8 @@ package com.simon;
 
 import com.simon.util.*;
 
-import java.util.*;
 import java.util.LinkedList;
-import java.util.stream.Collectors;
+import java.util.Queue;
 
 public class Main {
     private static Solution solution = new Solution();
@@ -22,11 +21,34 @@ public class Main {
 
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        return check(root, root);
+        return checkBFS(root, root);
     }
-    boolean check(TreeNode p, TreeNode q) {
+
+    boolean checkDFS(TreeNode p, TreeNode q) {
         if (p == null && q == null) return true;
         if (p == null || q == null) return false;
-        return p.val == q.val && check(p.left, q.right) && check(p.right, q.left);
+        return p.val == q.val && checkDFS(p.left, q.right) && checkDFS(p.right, q.left);
+    }
+
+    boolean checkBFS(TreeNode u, TreeNode v) {
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(u);
+        q.offer(v);
+        while (!q.isEmpty()) {
+            u = q.poll();
+            v = q.poll();
+            if (u == null && v == null) {
+                continue;
+            }
+            if ((u == null || v == null) || (u.val != v.val)) {
+                return false;
+            }
+            q.offer(u.left);
+            q.offer(v.right);
+
+            q.offer(u.right);
+            q.offer(v.left);
+        }
+        return true;
     }
 }
