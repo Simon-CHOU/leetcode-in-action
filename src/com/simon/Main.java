0b0d6815984abb386db0c0810b4e2e48436770c7
Simon-CHOU <mrsimonzhou@gmail.com>
2021-02-26 16:45:42 +0800

    617 merge-two-binary-trees(bfs)

--- src/com/simon/Main.java
+++ src/com/simon/Main.java
@@ -1,6 +1,7 @@
 package com.simon;
 
 import com.simon.util.*;
+import com.sun.source.tree.Tree;
 
 import java.util.*;
 import java.util.LinkedList;
@@ -13,22 +14,58 @@ public class Main {
         while (true) {
             TreeNode s = InputTreeUtil.inputBtree();
             TreeNode t = InputTreeUtil.inputBtree();
-            System.out.println(solution.mergeTrees(s, t));
+//            System.out.println();
+            DisplayTreeUtil.levelOrderTraversal(solution.mergeTrees(s, t));
         }
     }
 }
 
 class Solution {
-    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
-        if (root1 == null) {
-            return root2;
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null) {
+            return t2;
         }
-        if (root2 == null) {
-            return root1;
+        if (t2 == null) {
+            return t1;
         }
-        TreeNode merged = new TreeNode(root1.val + root2.val);
-        merged.left = mergeTrees(root1.left, root2.left);
-        merged.right = mergeTrees(root1.right, root2.right);
+        TreeNode merged = new TreeNode(t1.val + t2.val);
+        Queue<TreeNode> queue = new LinkedList<>();//合并后的树
+        Queue<TreeNode> queue1 = new LinkedList<>();
+        Queue<TreeNode> queue2 = new LinkedList<>();
+        queue.offer(merged);
+        queue1.offer(t1);
+        queue2.offer(t2);
+        while (!queue1.isEmpty() && !queue2.isEmpty()) {
+            TreeNode node = queue.poll(), node1 = queue1.poll(), node2 = queue2.poll();
+            TreeNode left1 = node1.left, right1 = node1.right, left2 = node2.left, right2 = node2.right;
+            if (left1 != null || left2 != null) {
+                if (left1 != null && left2 != null) {
+                    TreeNode left = new TreeNode(left1.val + left2.val);
+                    node.left = left;
+                    queue.offer(left);//各自遍历
+                    queue1.offer(left1);
+                    queue2.offer(left2);
+                } else if (left1 != null) {
+                    node.left = left1;
+                } else {//if(left2 != null){
+                    node.left = left2;
+                }
+            }
+            if (right1 != null || right2 != null) {
+                if (right1 != null && right2 != null) {
+                    TreeNode right = new TreeNode(right1.val + right2.val);
+                    node.right = right;
+                    queue.offer(right);//各自遍历
+                    queue1.offer(right1);
+                    queue2.offer(right2);
+                } else if (right1 != null) {
+                    node.right = right1;
+                } else {// if(right2 != null){
+                    node.right = right2;
+                }
+            }
+
+        }
         return merged;
     }
 }
