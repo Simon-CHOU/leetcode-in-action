6e655036255b294f91cde76732524f7a3c2ef485
Simon-CHOU <mrsimonzhou@gmail.com>
2021-03-05 17:31:45 +0800

    322 coin-change(暴力递归)
    res 一定要带着一起递归，不能设为Solution的成员

--- src/com/simon/Main.java
+++ src/com/simon/Main.java
@@ -11,31 +11,34 @@ public class Main {
     public static void main(String[] args) {
         while (true) {
 //            TreeNode s = InputTreeUtil.inputBtree();
+            int[] c = InputUtil.inputIntArray();
             int s = InputUtil.inputInt();
-            System.out.println(solution.fib(s));
+            System.out.println(solution.coinChange(c, s));
         }
     }
 }
 
 class Solution {
 
-    public int fib(int n) {
-        if (n == 0) return 0;
-        if (n == 1 || n == 2) return 1;
-        int prev = 1, curr = 1;
-        //根据斐波那契数列的状态转移方程，当前状态只和之前的两个状态有关，其实并不需要那么长的一个 DP table 来存储所有的状态，
-        // 只要想办法存储之前的两个状态就行了。所以，可以进一步优化，把空间复杂度降为 O(1)
-//        int[] dp = new int[n + 1];
-//        dp[1] = 1;
-//        dp[2] = 1;
-        for (int i = 3; i <= n; i++) {
-//            dp[i] = dp[i - 1] + dp[i - 2];
-            int sum = prev + curr;
-            prev = curr;
-            curr = sum;
-        }
-//        return dp[n];
-        return curr;
+    public int coinChange(int[] coins, int amount) {
+        return dp(amount, coins, Integer.MAX_VALUE);
     }
 
+    int dp(int n, final int[] coins, int res) {
+        if (n == 0) return 0;
+        if (n < 0) return -1;
+        for (int coin : coins) {
+            int subProblem = dp(n - coin, coins, res);
+            if (subProblem == -1) {//子问题无界，则跳过
+                continue;
+            }
+            res = Math.min(res, 1 + subProblem);
+        }
+        return res != Integer.MAX_VALUE ? res : -1;
+    }
 }
+/**
+ * [1,2,5]
+ * 11
+ * exp:3
+ */
