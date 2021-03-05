8e3237d455cfde00b6ca7bda1986e1cc8d7ba575
Simon-CHOU <mrsimonzhou@gmail.com>
2021-03-05 11:30:42 +0800

    338 counting-bits(动态规划 最低设置位)
    定义正整数 x 的「最低设置位」为 x 的二进制表示中的最低的 1 所在位。

--- src/com/simon/Main.java
+++ src/com/simon/Main.java
@@ -21,7 +21,7 @@ class Solution {
     public int[] countBits(int num) {
         int[] bits = new int[num + 1];
         for (int i = 1; i <= num; i++) {
-            bits[i] = bits[i>>1] +(i&1);
+            bits[i] = bits[i&(i-1)] +1;
         }
         return bits;
     }
