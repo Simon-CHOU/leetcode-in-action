e20e3c740d73a7eac4403da8421c3a8d61f61527
Simon-CHOU <mrsimonzhou@gmail.com>
2021-03-05 11:24:39 +0800

    338 counting-bits(动态规划 最低有效位)

--- src/com/simon/Main.java
+++ src/com/simon/Main.java
@@ -20,12 +20,8 @@ public class Main {
 class Solution {
     public int[] countBits(int num) {
         int[] bits = new int[num + 1];
-        int highBit = 0;
         for (int i = 1; i <= num; i++) {
-            if ((i & i - 1) == 0) {
-                highBit = i;
-            }
-            bits[i] = bits[i - highBit] + 1;
+            bits[i] = bits[i>>1] +(i&1);
         }
         return bits;
     }
