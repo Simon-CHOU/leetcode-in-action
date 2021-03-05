618ff7619ce7c0a68d34edcee66d382caa244c24
Simon-CHOU <mrsimonzhou@gmail.com>
2021-03-05 09:57:21 +0800

    338 counting-bits(直接法)

--- src/com/simon/Main.java
+++ src/com/simon/Main.java
@@ -10,46 +10,28 @@ public class Main {
 
     public static void main(String[] args) {
         while (true) {
-            TreeNode s = InputTreeUtil.inputBtree();
-            System.out.println(solution.zigzagLevelOrder(s));
+//            TreeNode s = InputTreeUtil.inputBtree();
+            int s = InputUtil.inputInt();
+            DisplayArrayUtil.disp(solution.countBits(s));
         }
     }
 }
 
 class Solution {
-    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<List<Integer>> ans = new ArrayList<>();
-        dfs(root, ans, 0);
-        return ans;
+    public int[] countBits(int num) {
+        int[] bits = new int[num + 1];
+        for (int i = 0; i <= num; i++) {
+            bits[i] = countOnes(i);
+        }
+        return bits;
     }
 
-    /**
-     * DFS遍历本身与正常的DFS并无区别，改造在于带着level+List，维护要输出的答案
-     *
-     * @param node  当前结点
-     * @param ans   目标List
-     * @param level 当前层数
-     */
-    void dfs(TreeNode node, List<List<Integer>> ans, int level) {
-        if (node == null) {
-            return;
+    public int countOnes(int x) {
+        int ones = 0;
+        while (x > 0) {
+            x &= (x - 1);//抹去最后一位1
+            ones++;//多少次抹干净，就有多少个1
         }
-        if (ans.size() <= level) {//ans.size与层数对应，ans.size()<=level,即下一层and的List还未创建
-            List<Integer> newLevel = new LinkedList<>();
-            ans.add(newLevel);
-        }
-        Deque<Integer> list = new ArrayDeque<>(ans.get(level));
-        if (level % 2 == 0) {//层数的奇偶，控制zigzag
-//            list.off(node.val);//偶数层从做到右
-            list.offerLast(node.val);
-        } else {
-//            list.add(0, node.val);//add(0,val)在首位插入，跟双端队列是一回事
-            list.offerFirst(node.val);
-        }
-        ans.set(level, new ArrayList<>(list));
-        dfs(node.left, ans, level + 1);
-        dfs(node.right, ans, level + 1);
-
+        return ones;
     }
 }
-//[3,9,20,null,null,15,7]
---
+++ src/com/simon/util/DisplayArrayUtil.java
@@ -0,0 +1,11 @@
+package com.simon.util;
+
+public class DisplayArrayUtil {
+    public static void disp(int[] arr) {
+        System.out.print("[");
+        for (int i = 0; i < arr.length - 1; i++) {
+            System.out.print(arr[i] + ",");
+        }
+        System.out.println(arr[arr.length - 1] + "]");
+    }
+}
