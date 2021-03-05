1f80dca0e05d427773722c9c217b871c3b2d18a5
Simon-CHOU <mrsimonzhou@gmail.com>
2021-03-05 15:55:04 +0800

    509 fibonacci-number(动态规划+空间优化)

--- src/com/simon/Main.java
+++ src/com/simon/Main.java
@@ -22,13 +22,20 @@ class Solution {
     public int fib(int n) {
         if (n == 0) return 0;
         if (n == 1 || n == 2) return 1;
-        int[] dp = new int[n + 1];
-        dp[1] = 1;
-        dp[2] = 1;
+        int prev = 1, curr = 1;
+        //根据斐波那契数列的状态转移方程，当前状态只和之前的两个状态有关，其实并不需要那么长的一个 DP table 来存储所有的状态，
+        // 只要想办法存储之前的两个状态就行了。所以，可以进一步优化，把空间复杂度降为 O(1)
+//        int[] dp = new int[n + 1];
+//        dp[1] = 1;
+//        dp[2] = 1;
         for (int i = 3; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+//            dp[i] = dp[i - 1] + dp[i - 2];
+            int sum = prev + curr;
+            prev = curr;
+            curr = sum;
         }
-        return dp[n];
+//        return dp[n];
+        return curr;
     }
 
 }
