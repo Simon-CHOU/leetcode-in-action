66183f308080a7c5382d698c73166c058c9d3531
Simon-CHOU <mrsimonzhou@gmail.com>
2021-03-08 16:11:55 +0800

    509 coin-change(带备忘录的递归)
    暴力递归会超时

--- src/com/simon/Main.java
+++ src/com/simon/Main.java
@@ -19,26 +19,38 @@ public class Main {
 }
 
 class Solution {
+    // HashMap<Integer, Integer> memo = new HashMap<>();//HashMap没有int数组块
+    int[] memo;// 带备忘录的递归
 
     public int coinChange(int[] coins, int amount) {
-        return dp(amount, coins, Integer.MAX_VALUE);
+        memo = new int[amount + 1];//第一位不用
+        return dp(amount, coins);
     }
 
-    int dp(int n, final int[] coins, int res) {
-        if (n == 0) return 0;
+    int dp(int n, final int[] coins) {
         if (n < 0) return -1;
+        if (n == 0) return 0;
+        if (memo[n] != 0) {
+            return memo[n];
+        }
+        int res = Integer.MAX_VALUE;// res 不要带着一起递归
         for (int coin : coins) {
-            int subProblem = dp(n - coin, coins, res);
-            if (subProblem == -1) {//子问题无界，则跳过
+            int subProblem = dp(n - coin, coins);
+            if (subProblem == -1) {//子问题无解，则跳过
                 continue;
             }
             res = Math.min(res, 1 + subProblem);
         }
-        return res != Integer.MAX_VALUE ? res : -1;
+        memo[n] = res != Integer.MAX_VALUE ? res : -1;
+        return memo[n];
     }
 }
 /**
  * [1,2,5]
  * 11
  * exp:3
+ * <p>
+ * [186,419,83,408]
+ * 6249
+ * exp:20
  */
