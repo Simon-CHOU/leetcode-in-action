096eef37a5930b2acf53d006bc7c3ac273145f69
Simon-CHOU <mrsimonzhou@gmail.com>
2021-03-15 14:46:44 +0800

    146 lru-cache(HashMap+双向链表)

--- src/com/simon/Main.java
+++ src/com/simon/Main.java
@@ -18,25 +18,86 @@ public class Main {
     }
 }
 
-class LRUCache extends LinkedHashMap<Integer, Integer> {
-    private int capacity;
+class LRUCache {
+    class DLinkedNode {
+        int key;
+        int value;
+        DLinkedNode prev;
+        DLinkedNode next;
+
+        public DLinkedNode() {
+        }
+
+        public DLinkedNode(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private Map<Integer, DLinkedNode> cache = new HashMap<>();
+    int size;//当前已消耗容量
+    private int capacity;//LRU容器总容量
+    private DLinkedNode head, tail;
 
     public LRUCache(int capacity) {
-        super(capacity, 0.75F, true);
+        this.size = 0;
         this.capacity = capacity;
+        head = new DLinkedNode();
+        tail = new DLinkedNode();
+        head.next = tail;
+        tail.prev = head;
     }
 
     public int get(int key) {
-        return super.getOrDefault(key, -1);
+        DLinkedNode node = cache.get(key);
+        if (node == null) {
+            return -1;
+        }
+        moveToHead(node);
+        return node.value;
     }
+
 
     public void put(int key, int value) {
-        super.put(key, value);
+        DLinkedNode node = cache.get(key);
+        if (node == null) {
+            DLinkedNode newNode = new DLinkedNode(key, value);
+            cache.put(key, newNode);
+            addToHead(newNode);
+            ++size;
+            if (size > capacity) {
+                DLinkedNode tail = removeTail();
+                cache.remove(tail.key);
+                --size;
+            }
+        } else {
+            node.value = value;
+            moveToHead(node);
+        }
+
     }
 
-    @Override
-    protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
-        return size() > capacity;
+    void moveToHead(DLinkedNode node) {
+        removeNode(node);
+        addToHead(node);
+    }
+
+    void removeNode(DLinkedNode node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    void addToHead(DLinkedNode node) {
+        node.prev = head;
+        node.next = head.next;
+        head.next.prev = node;
+        head.next = node;
+    }
+
+    DLinkedNode removeTail() {
+        DLinkedNode res = tail.prev;
+        removeNode(res);
+        return res;
     }
 }
 /**
