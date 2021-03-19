d03ed2cec359c6de7c54fd15fdf5b350ba98aa36
Simon-CHOU <mrsimonzhou@gmail.com>
2021-03-20 00:35:23 +0800

    3 longest-substring-without-repeating-characters(滑动窗口HashMap解)

--- src/com/simon/Main.java
+++ src/com/simon/Main.java
@@ -10,27 +10,67 @@ public class Main {
 
     public static void main(String[] args) {
         while (true) {
-            String s = InputUtil.inputStr();
-            System.out.println(solution.lengthOfLongestSubstring(s));
+//            String s = InputUtil.inputStr();
+//            System.out.println(solution.lengthOfLongestSubstring(s));
+            System.out.println(solution.lengthOfLongestSubstring("pwwkew"));
         }
     }
 }
 
 class Solution {
+
+
+//    public int lengthOfLongestSubstring(String s) {
+//        int n = s.length(), ans = 0;
+//        Map<Character, Integer> map = new HashMap<>();
+//        for (int end = 0, start = 0; end < n; end++) {
+//            char alpha = s.charAt(end);
+//            if (map.containsKey(alpha)) {
+//                start = Math.max(map.get(alpha), start);
+//            }
+//            ans = Math.max(ans, end - start + 1);
+//            map.put(s.charAt(end), end + 1);
+//        }
+//        return ans;
+//    }
+
+
     public int lengthOfLongestSubstring(String s) {
-        Set<Character> occ = new HashSet<>();
-        int n = s.length();
-        int rk = -1, res = 0;
-        for (int i = 0; i < n; i++) {
-            if (i != 0) {
-                occ.remove(s.charAt(i - 1));
+        if (s.length() == 0) return 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+        int res = 0;
+        int left = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))) {
+                left = Math.max(left, map.get(s.charAt(i)) + 1);
             }
-            while (rk + 1 < n && !occ.contains(s.charAt(rk + 1))) {
-                occ.add(s.charAt(rk + 1));
-                ++rk;
-            }
-            res = Math.max(res, rk - i + 1);
+            map.put(s.charAt(i), i);
+            int b = i - left + 1;
+            res = Math.max(res, b);
         }
         return res;
     }
+
+//public int lengthOfLongestSubstring(String s) {
+//    // 哈希集合，记录每个字符是否出现过
+//    Set<Character> occ = new HashSet<Character>();
+//    int n = s.length();
+//    // 右指针，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
+//    int rk = -1, ans = 0;
+//    for (int i = 0; i < n; ++i) {
+//        if (i != 0) {
+//            // 左指针向右移动一格，移除一个字符
+//            occ.remove(s.charAt(i - 1));
+//        }
+//        while (rk + 1 < n &&
+//                !occ.contains(s.charAt(rk + 1))) {
+//            // 不断地移动右指针
+//            occ.add(s.charAt(rk + 1));
+//            ++rk;
+//        }
+//        // 第 i 到 rk 个字符是一个极长的无重复字符子串
+//        ans = Math.max(ans, rk - i + 1);
+//    }
+//    return ans;
+//}
 }
