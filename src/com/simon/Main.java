51fe697145dcc2d5fb4a636bd4b3fbbec88d8792
Simon-CHOU <mrsimonzhou@gmail.com>
2021-05-26 15:01:12 +0800

    122. 买卖股票的最佳时机 II

--- src/com/simon/Main.java
+++ src/com/simon/Main.java
@@ -18,17 +18,32 @@ public class Main {
 
 class Solution {
     public int maxProfit(int[] prices) {
-        int maxProfit = 0;
-        int minPrice = Integer.MAX_VALUE;
-        for (int price : prices) {
-            if (price < minPrice) {
-                minPrice = price;
-            } else {
-                if (price - minPrice > maxProfit) {
-                    maxProfit = price - minPrice;
-                }
-            }
+        int n = prices.length;
+        int dp0 = 0, dp1 = -prices[0];
+        for (int i = 1; i < n; i++) {
+            // dp0 当天结束不持有（昨天就卖了or卖了昨天的）
+            int newDp0 = Math.max(dp0, dp1 + prices[i]);//max(昨天就没有今天也不买,今天卖出)
+            // dp1 当天结束持有（昨天就买了or今天刚买）
+            int newDp1 = Math.max(dp1, dp0 - prices[i]);//max(昨天就持有今天不动，今天买入)
+            dp0 = newDp0;
+            dp1 = newDp1;
         }
-        return maxProfit;
+        return dp0;
     }
+    /**
+     *  public int maxProfit(int[] prices) {
+     *         int n = prices.length;
+     *         int[][] dp = new int[n][2];
+     *         dp[0][0] = 0;
+     *         dp[0][1] = -prices[0];
+     *         int maxProfit = 0;
+     *         for (int i = 1; i < n; i++) {
+     *             // [x][0] 当天结束不持有（昨天就卖了or卖了昨天的）
+     *             dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);//max(昨天就没有今天也不买,今天卖出)
+     *             // [x][1] 当天结束持有（昨天就买了or今天刚买）
+     *             dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);//max(昨天就持有今天不动，今天买入)
+     *         }
+     *         return dp[n - 1][0];
+     *     }
+     */
 }
