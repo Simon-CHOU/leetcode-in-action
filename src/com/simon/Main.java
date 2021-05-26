0ce1da09fbb0bcfd4ba5ceec91febbaba8a12fc1
Simon-CHOU <mrsimonzhou@gmail.com>
2021-05-26 20:02:07 +0800

    217. 存在重复元素

--- src/com/simon/Main.java
+++ src/com/simon/Main.java
@@ -11,68 +11,24 @@ public class Main {
     public static void main(String[] args) {
         while (true) {
             int[] nums = InputUtil.inputIntArray();
-            int k = InputUtil.inputInt();
-            solution.rotate(nums, k);
+            solution.containsDuplicate(nums);
         }
     }
 }
 
 class Solution {
-    public void rotate(int[] nums, int k) {
-        int len = nums.length;
-        k = k % len;
-        reverseArr(nums, 0, len-1);
-        reverseArr(nums, 0, k - 1);
-        reverseArr(nums, k, len - 1);
-    }
-
-    /**
-     * reverse arr[start]~arr[end], includ  arr[start] & arr[end]
-     *
-     * @param arr   array
-     * @param start begin
-     * @param end   end
-     */
-    private void reverseArr(int[] arr, int start, int end) {
-        while(start<end){
-            int temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-            start++;
-            end--;
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i + 1] == nums[i])
+                return true;
         }
-//        int len = end - start + 1;
-//        int mid = start + (len / 2) -1;
-//        for (int i = start; i < mid; i++) {
-//            int temp = arr[i];
-//            arr[i] = arr[len - 1 - i];
-//            arr[len - 1 - i] = temp;
+        return false;
+//        Set<Integer> filter = new HashSet<>();
+//        for (int i = 0; i < nums.length; i++) {
+//            if(!filter.add(nums[i]))
+//                return true;
 //        }
+//        return false;
     }
-    /** arrycopy 简化
-     *     public void rotate(int[] nums, int k) {
-     *         int len = nums.length;
-     *         int[] ans = new int[len];
-     *         for (int i = 0; i < len; i++) {
-     *             ans[(i + k) % len] = nums[i];
-     *         }
-     *         System.arraycopy(ans,0,nums,0, len);
-     * //        for (int i = 0; i < len; i++) {
-     * //            nums[i] = ans[i];
-     * //        }
-     *
-     *     }
-     */
-    /** 自解
-     *     public void rotate(int[] nums, int k) {
-     *         int len = nums.length;
-     *         int[] ans = new int[len];
-     *         for (int i = 0; i < len; i++) {
-     *             ans[(i + k) % len] = nums[i];
-     *         }
-     *         for (int i = 0; i < len; i++) {
-     *             nums[i] = ans[i];
-     *         }
-     *     }
-     */
 }
