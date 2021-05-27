977212c2bd01854d8af9c4d725a54d78826e34f4
Simon-Chou <mrsimonzhou@gmail.com>
2021-05-28 06:53:55 +0800

    283. 移动零

--- src/com/simon/Main.java
+++ src/com/simon/Main.java
@@ -12,28 +12,30 @@ public class Main {
 
     public static void main(String[] args) {
         while (true) {
-            int[] nums = InputUtil.inputIntArray();
-            DisplayArrayUtil.disp(solution.plusOne(nums));
+//            int[] nums = InputUtil.inputIntArray();
+            int[] nums = {0,1,0,3,12};
+//            int[] nums = new int[]{0,1,0,3,12};
+            solution.moveZeroes(nums);
+            DisplayArrayUtil.disp(nums);
         }
     }
 }
 
 class Solution {
-    /**
-     * 关键：只加1.
-     *
-     * @param digits
-     * @return
-     */
-    public int[] plusOne(int[] digits) {
-        int len = digits.length;
-        for (int i = len - 1; i >= 0; i--) {
-            digits[i]++;
-            digits[i] = digits[i] % 10;//除了9 加1进位(且原位为0），其他都不进位
-            if (digits[i] != 0) return digits;
+    public void moveZeroes(int[] nums) {
+        int l = 0, r = 0;
+        while (r < nums.length) {
+            if (nums[r] != 0) {
+                swap(nums, l, r);
+                l++;
+            }
+            r++;
         }
-        digits = new int[len + 1];//处理99,999..等特殊情况，进位1，且其余都是0
-        digits[0] = 1;
-        return digits;
+    }
+
+    private void swap(int[] nums, int l, int r) {
+        int t = nums[l];
+        nums[l] = nums[r];
+        nums[r] = t;
     }
 }
