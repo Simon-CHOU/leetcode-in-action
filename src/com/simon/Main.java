7b1de7e7794bc64ca2da471abe16c4b2ba457a75
Simon-CHOU <mrsimonzhou@gmail.com>
2021-05-28 09:38:24 +0800

    350. 两个数组的交集 II(HashMap)

--- src/com/simon/Main.java
+++ src/com/simon/Main.java
@@ -11,24 +11,38 @@ public class Main {
     public static void main(String[] args) {
         while (true) {
             int[] nums = InputUtil.inputIntArray();
-            solution.containsDuplicate(nums);
+            int[] nums1 = InputUtil.inputIntArray();
+            DisplayArrayUtil.disp(solution.intersect(nums, nums1));
         }
     }
 }
 
 class Solution {
-    public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (nums[i + 1] == nums[i])
-                return true;
+    public int[] intersect(int[] nums1, int[] nums2) {
+        //nums1 short, nums2 long
+        if (nums1.length > nums2.length) {
+            return intersect(nums2, nums1);// bravo~
         }
-        return false;
-//        Set<Integer> filter = new HashSet<>();
-//        for (int i = 0; i < nums.length; i++) {
-//            if(!filter.add(nums[i]))
-//                return true;
-//        }
-//        return false;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int n : nums1) {
+            int count = map.getOrDefault(n, 0) + 1;
+            map.put(n, count);
+        }
+        int[] intersection = new int[nums1.length];
+        int index = 0;
+        for (int num : nums2) {
+            int count = map.getOrDefault(num, 0);
+            if (count > 0) {
+                intersection[index++] = num;
+                count--;
+                if (count > 0) {
+                    map.put(num, count);//refresh count vlaue
+                } else {
+                    map.remove(num);
+                }
+            }
+        }
+        return Arrays.copyOfRange(intersection, 0, index);
     }
+
 }
