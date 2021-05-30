305060e3f60b1d318c9539f65f53463ff20aea86
Simon-Chou <mrsimonzhou@gmail.com>
2021-05-31 01:37:05 +0800

    36. 有效的数独(HashMap[] 一次遍历)
    由于9x9数目不大，相比数组三次遍历，一次遍历效率提升不明显。

--- src/com/simon/Main.java
+++ src/com/simon/Main.java
@@ -18,43 +18,28 @@ public class Main {
 
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        //数字 1-9 在每一行只能出现一次。
+        HashMap<Integer, Integer>[] rows = new HashMap[9];//hashmap 也能二维！
+        HashMap<Integer, Integer>[] columns = new HashMap[9];//hashmap 也能二维！
+        HashMap<Integer, Integer>[] boxes = new HashMap[9];//hashmap 也能二维！
         for (int i = 0; i < 9; i++) {
-            Map<Character, Integer> map = new HashMap<>();
+            rows[i] = new HashMap<>();
+            columns[i] = new HashMap<>();
+            boxes[i] = new HashMap<>();
+        }
+        //valid a board
+        for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
-                if (board[i][j] == '.') continue;
-                if (map.get(board[i][j]) != null) {
-                    return false;
-                } else {
-                    map.put(board[i][j], 1);
-                }
-            }
-        }
-        //数字 1-9 在每一列只能出现一次。
-        for (int j = 0; j < 9; j++) {
-            Map<Character, Integer> map = new HashMap<>();
-            for (int i = 0; i < 9; i++) {
-                if (board[i][j] == '.') continue;
-                if (map.get(board[i][j]) != null) {
-                    return false;
-                } else {
-                    map.put(board[i][j], 1);
-                }
-            }
-        }
-        //数字 1-9 在每一个以粗实线分隔的 3x3 宫内只能出现一次。
-        Map<Character, Integer> map = new HashMap<>();
-        for (int p = 0; p <= 6; p+=3) {
-            for (int q = 0; q <= 6; q+=3) {
-                map.clear();
-                for (int j = p; j < p+3; j++) {
-                    for (int i = q; i < q+3; i++) {
-                        if (board[i][j] == '.') continue;
-                        if (map.get(board[i][j]) != null) {
-                            return false;
-                        } else {
-                            map.put(board[i][j], 1);
-                        }
+                char num = board[i][j];
+                if (num != '.') {
+                    int n = (int) num;
+                    int box_index = (i / 3) * 3 + j / 3;
+                    //keep the current cell value
+                    rows[i].put(n, rows[i].getOrDefault(n, 0) + 1);//getOrDefault  比  if( get ==null) set count++ 更简洁
+                    columns[j].put(n, columns[j].getOrDefault(n, 0) + 1);
+                    boxes[ box_index].put(n, boxes[box_index].getOrDefault(n, 0) + 1);
+
+                    if (rows[i].get(n) > 1 || columns[j].get(n) > 1 || boxes[box_index].get(n) > 1) {
+                        return false;
                     }
                 }
             }
@@ -64,17 +49,17 @@ class Solution {
 }
 
 /**
-[["5","3",".",".","7",".",".",".","."]
-,["6",".",".","1","9","5",".",".","."]
-,[".","9","8",".",".",".",".","6","."]
-,["8",".",".",".","6",".",".",".","3"]
-,["4",".",".","8",".","3",".",".","1"]
-,["7",".",".",".","2",".",".",".","6"]
-,[".","6",".",".",".",".","2","8","."]
-,[".",".",".","4","1","9",".",".","5"]
-,[".",".",".",".","8",".",".","7","9"]]
-<p>
-来源：力扣（LeetCode）
-链接：https://leetcode-cn.com/problems/valid-sudoku
-著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ [["5","3",".",".","7",".",".",".","."]
+ ,["6",".",".","1","9","5",".",".","."]
+ ,[".","9","8",".",".",".",".","6","."]
+ ,["8",".",".",".","6",".",".",".","3"]
+ ,["4",".",".","8",".","3",".",".","1"]
+ ,["7",".",".",".","2",".",".",".","6"]
+ ,[".","6",".",".",".",".","2","8","."]
+ ,[".",".",".","4","1","9",".",".","5"]
+ ,[".",".",".",".","8",".",".","7","9"]]
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/valid-sudoku
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
