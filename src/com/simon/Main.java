10a604c83068d6ff1a4e743443508c5791244b3a
Simon-Chou <mrsimonzhou@gmail.com>
2021-06-02 02:44:59 +0800

    8. 字符串转换整数 (atoi) 官解自动机

--- src/com/simon/Main.java
+++ src/com/simon/Main.java
@@ -18,88 +18,46 @@ public class Main {
 
 class Solution {
     public int myAtoi(String s) {
-        long ans = 0;
-        s = s.trim();//去掉空格，主要是前导空格
-        if(s.length()<1){
-            return (int)ans;// 特殊处理，空字符串
+        Automaton automaton = new Automaton();
+        int length = s.length();
+        for (int i = 0; i < length; i++) {
+            automaton.get(s.charAt(i));
         }
-////        int k;
-//        while( s.length()>0 ) {
-//            if (s.charAt(0) != '+'&&
-//                    s.charAt(0) != '-'&& !Character.isDigit(s.charAt(0))) {
-//                s = s.substring(1);//去掉开头数字 +-  "words and 987"
-////                i++;
-//            } else {
-//                break;
-//            }
-//        }
-        while( s.length()>0 ) {
-            if (s.charAt(0) == '0') {
-                s = s.substring(1);//去掉前导0
-            } else {
-                break;
-            }
-        }
-        //处理符号
-        int flag = 1;
-        //检查数字的实际符号, e.g. "-+12", "   -43"
-//        int len = s.length();
-//        while (len > 0) {
-            char firstCh = s.charAt(0);
-//            if(!Character.isDigit(firstCh)&&firstCh!='-'&&firstCh!='+'){
-//                break;
-//            }
-//            if (Character.isDigit(firstCh)) {
-//                break;
-//            }
-//            if (firstCh == '-') {
-//                flag = -1;
-//                s = s.substring(1);
-//            }
-//            if (firstCh == '+') {
-//                flag = 1;
-//                s = s.substring(1);
-//            }
-//            len = s.length();
-//        }
-        if (firstCh == '-') {
-            flag = -1;
-            s = s.substring(1);
-        }
-        if (firstCh == '+') {
-            flag = 1;
-            s = s.substring(1);
-        }
-
-
-        //检查数字字符串末尾
-        int end = s.length() - 1;
-        for (int i = 0; i < s.length(); i++) {
-            if (!Character.isDigit(s.charAt(i))) {
-                end = i - 1;
-                break;//非数字字符，丢弃字符串后续部分
-            }
-        }
-        //转换数字
-        for (int i = 0; i <= end; i++) {
-            if (flag * ans >= Integer.MAX_VALUE) {
-                return Integer.MAX_VALUE;//校验范围。
-            }
-            if (flag * ans <= Integer.MIN_VALUE) {
-                return Integer.MIN_VALUE;
-            }
-            ans = ans * 10 + s.charAt(i) - '0';
-        }
-        if(flag* ans>=Integer.MAX_VALUE){
-            return  Integer.MAX_VALUE;
-        }
-        if(flag *ans<=Integer.MIN_VALUE){
-            return  Integer.MIN_VALUE;
-        }
-        return (int) (flag * ans);
+        return (int) (automaton.sign * automaton.ans);
     }
 }
-///"+-12"  exp: 0
-// words and 987  exp: 0
-// "21474836460" exp: 2147483647
-//"00000-42a1234"  exp: 0
+
+class Automaton {
+    public int sign = 1;
+    public long ans = 0;
+    private String state = "start";
+    private Map<String, String[]> table = new HashMap<>() {{
+        put("start", new String[]{"start", "signed", "in_number", "end"});
+        put("signed", new String[]{"end", "end", "in_number", "end"});
+        put("in_number", new String[]{"end", "end", "in_number", "end"});
+        put("end", new String[]{"end", "end", "end", "end"});// 四列分别表示接受的char ' ',	+/-, number, other
+    }};//用表格表示自动机
+
+    public void get(char c) {
+        state = table.get(state)[get_col(c)];
+        if ("in_number".equals(state)) {
+            ans = ans * 10 + c - '0';
+            ans = sign == 1 ? Math.min(ans, (long) Integer.MAX_VALUE) : Math.min(ans, -(long) Integer.MIN_VALUE);
+        } else if ("signed".equals(state)) {
+            sign = c == '+' ? 1 : -1;
+        }
+    }
+
+    public int get_col(char c) {
+        if (c == ' ') {
+            return 0;
+        }
+        if (c == '+' || c == '-') {
+            return 1;
+        }
+        if (Character.isDigit(c)) {
+            return 2;
+        }
+        return 3;
+    }
+}
