e447d687b34b24c60306737c76868818b45f97ad
Simon-CHOU <mrsimonzhou@gmail.com>
2021-06-01 11:42:22 +0800

    242. 有效的字母异位词

--- src/com/simon/Main.java
+++ src/com/simon/Main.java
@@ -10,25 +10,65 @@ public class Main {
 
     public static void main(String[] args) {
         while (true) {
-            char[] arr = InputUtil.inputCharArr();
-            solution.reverseString(arr);
+            String s = InputUtil.inputStr();
+            String s1 = InputUtil.inputStr();
+            System.out.println(solution.isAnagram(s, s1));
         }
     }
 }
 
 class Solution {
-    public void reverseString(char[] s) {
-        int length = s.length;
-        for (int left = 0, right = length - 1 - left; left < right; left++, right--) {
-            char temp = s[left];
-            s[left] = s[right];
-            s[right] = temp;
+    //hash(unicode) 慢，兼容性好
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {// 防用例： ab, a
+            return false;
         }
-//        for (int i = 0; i < s.length / 2; i++) {
-//            char temp = s[i];
-//            s[i] = s[s.length - 1 - i];
-//            s[s.length - 1 - i] = temp;
-//        }
-//        System.out.println(s);
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+        for (int i = 0; i < t.length(); i++) {
+            char key = t.charAt(i);
+            Integer currentCount = map.getOrDefault(key, 0);
+            if (currentCount == 0) {
+                return false;
+            }
+            map.put(key, currentCount - 1);
+        }
+        return true;
     }
+
+    //hash(仅ASCII字符) 次快
+//    public boolean isAnagram(String s, String t) {
+//        if (s.length() != t.length()) {
+//            return false;
+//        }
+//        int[] table = new int[26];
+//        for (int i = 0; i < s.length(); i++) {
+//            table[s.charAt(i) - 'a']++;
+//        }
+//        for (int i = 0; i < t.length(); i++) {
+//            table[t.charAt(i) - 'a']--;
+//            if (table[t.charAt(i) - 'a'] < 0) {
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
+
+    //sort 快
+//    public boolean isAnagram(String s, String t) {
+//        if (s.length() != t.length()) {
+//            return false;
+//        }
+//        char[] sChars = s.toCharArray();
+//        char[] tChars = t.toCharArray();
+//        Arrays.sort(sChars);
+//        Arrays.sort(tChars);
+//        if (Arrays.equals(sChars, tChars)) {
+//            return true;
+//        }
+//        return false;
+//    }
 }
