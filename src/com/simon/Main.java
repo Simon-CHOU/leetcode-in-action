1651d2ed5e5c6f4ee12dd392a2a4e29fb608b1d1
Simon-CHOU <mrsimonzhou@gmail.com>
2021-09-13 00:28:01 +0800

    15 3sum

--- src/com/simon/Main.java
+++ src/com/simon/Main.java
@@ -10,21 +10,57 @@ public class Main {
 
     public static void main(String[] args) {
         while (true) {
-            int[] arr = InputUtil.inputIntArray();// [-2,1,-3,4,-1,2,1,-5,4]
-            int i = solution.maxSubArray(arr);
-            System.out.println(i);
+            int[] arr = InputUtil.inputIntArray();// [-1,0,1,2,-1,-4]
+            solution.threeSum(arr);
 
         }
     }
 }
 
 class Solution {
-    public int maxSubArray(int[] nums) { // Dynamic Programming
-        int pre = 0, maxAns = nums[0];
-        for (int x : nums) {
-            pre = Math.max(pre + x, x); // f(i)=max{f(i−1)+nums[i],nums[i]}
-            maxAns = Math.max(maxAns, pre);
+    public List<List<Integer>> threeSum(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        // 枚举 a
+        for (int first = 0; first < n; ++first) {
+            // 需要和上一次枚举的数不相同
+            if (first > 0 && nums[first] == nums[first - 1]) {
+                continue;
+            }
+            // c 对应的指针初始指向数组的最右端
+            int third = n - 1;
+            int target = -nums[first];
+            // 枚举 b
+            for (int second = first + 1; second < n; ++second) {
+                // 需要和上一次枚举的数不相同
+                if (second > first + 1 && nums[second] == nums[second - 1]) {
+                    continue;
+                }
+                // 需要保证 b 的指针在 c 的指针的左侧
+                while (second < third && nums[second] + nums[third] > target) {
+                    --third;
+                }
+                // 如果指针重合，随着 b 后续的增加
+                // 就不会有满足 a+b+c=0 并且 b<c 的 c 了，可以退出循环
+                if (second == third) {
+                    break;
+                }
+                if (nums[second] + nums[third] == target) {
+                    List<Integer> list = new ArrayList<Integer>();
+                    list.add(nums[first]);
+                    list.add(nums[second]);
+                    list.add(nums[third]);
+                    ans.add(list);
+                }
+            }
         }
-        return maxAns;
+        return ans;
     }
 }
+
+//
+//作者：LeetCode-Solution
+//        链接：https://leetcode-cn.com/problems/3sum/solution/san-shu-zhi-he-by-leetcode-solution/
+//        来源：力扣（LeetCode）
+//        著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
