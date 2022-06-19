39766fb17d77ecfd97de395e31b3b460347917a3
Simon-CHOU <mrsimonzhou@gmail.com>
2022-06-20 01:18:04 +0800

    牛客：[编程题]删除链表的倒数第n个节点

--- src/com/simon/nowcoder/Main.java
+++ src/com/simon/nowcoder/Main.java
@@ -1,53 +1,103 @@
 package com.simon.nowcoder;
 
+import java.util.Scanner;
+
 /**
- * 01 背包，动态规划&最优解回溯
- * https://blog.csdn.net/qq_38410730/article/details/81667885
+ * [编程题]删除链表的倒数第n个节点
+ * https://www.nowcoder.com/questionTerminal/f95dcdafbde44b22a6d741baf71653f6
  */
 public class Main {
-    static int[] w = {0, 2, 3, 4, 5};
-    static int[] v = {0, 3, 4, 5, 6};
-    static int bagCapacity = 8;
-    static int[][] dp = new int[5][9];
-    static int[] item = new int[5]; // optimal solution
+    static Scanner sc = new Scanner(System.in);
+    static Solution solution = new Solution();
+    static Solution1 solution1 = new Solution1();
 
     public static void main(String[] args) {
-        for (int i = 1; i <= 4; i++) {
-            for (int j = 1; j <= bagCapacity; j++) {
-                if (j - w[i] < 0) {
-                    dp[i][j] = dp[i - 1][j];
-                } else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - w[i]] + v[i]);
-                }
-            }
-        }
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 9; j++) {
-                if (j == 8) { // last item of row
-                    System.out.print(dp[i][j] + "\n");
-                } else {
-                    System.out.print(dp[i][j] + " ");
-                }
-            }
-        }
-        recall(4,8);
-        for (int i = 0; i < 5; i++) {
-            if(item[i] == 1){
-                System.out.print("get " + i +" ");
-            }
-        }
+        ListNode n5 = new ListNode(5);
+        ListNode n4 = new ListNode(4, n5);
+        ListNode n3 = new ListNode(3, n4);
+        ListNode n2 = new ListNode(2, n3);
+        ListNode n1 = new ListNode(1, n2);
+        // 1->2->3->4->5, n= 2
+        int n = sc.nextInt();
+
+//        solution.removeNthFromEnd(n1, n);
+        solution1.removeNthFromEnd(n1, n);
+
+        displayList(n1); // n=2 res:   1->2->3->5
+        // n=3 res:   1->2->4->5
+
     }
 
-    static void recall(int i, int j) {
-        if (i > 0) {
-            if (dp[i][j] == dp[i - 1][j]) {
-                item[i] = 0;
-                recall(i - 1, j);
-            } else if (j - w[i] >= 0 &&
-                    dp[i][j] == dp[i - 1][j - w[i]] + v[i]) {
-                item[i] = 1;
-                recall(i - 1, j - w[i]);
-            }
+    public static void displayList(ListNode head) {
+        while (head.next != null) {
+            System.out.print(head.val + "->");
+            head = head.next;
         }
+        System.out.println(head.val); // error : head.next 1->2->3->4->null
+    }
+
+}
+
+class ListNode {
+    int val;
+    ListNode next = null;
+
+    public ListNode() {
+    }
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+}
+
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode first = head;
+        ListNode second = head;
+        for (int i = 0; i < n; i++)
+            first = first.next;
+        //如果n的值等于链表的长度,直接返回去掉头结点的链表
+        if (first == null)
+            return head.next;
+        while (first.next != null)   //同时移动两个指针
+        {
+            first = first.next;
+            second = second.next;
+        }
+        second.next = second.next.next;
+        return head;
+    }
+}
+
+// 思路1：双指针法
+// https://blog.csdn.net/qq_35398517/article/details/114492012
+class Solution1 {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode();//借助虚拟指针
+        dummy.next = head;
+        ListNode first = dummy;
+        ListNode second = dummy;
+        for (int i = 1; i <= n + 1; i++) {
+            first = first.next;
+        }
+        while (first != null) {
+            first = first.next;
+            second = second.next;
+        }
+        second.next = second.next.next;
+        return dummy.next;
     }
 }
