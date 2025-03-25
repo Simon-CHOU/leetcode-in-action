e53a4105cba2f254440be04f2ce6d01b452a82bb
Simon-CHOU <mrsimonzhou@gmail.com>
2025-03-25 14:10:59 +0800

    234. 回文链表（题解递归，字符串解法优化）
    https://leetcode.cn/problems/palindrome-linked-list/description/
    
    字符串reverse 是最容易懂的。

--- src/com/simon/tw/PalindromeLinkedList.java
+++ src/com/simon/tw/PalindromeLinkedList.java
@@ -2,24 +2,68 @@ package com.simon.tw;
 
 import com.simon.util.ListNode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution234 {
+    ListNode frontP = new ListNode();
+
     public boolean isPalindrome(ListNode head) {
+        frontP = head;
+        return isPalindromeCheck(head);
+    }
+
+    public boolean isPalindromeCheck(ListNode head) {
+        if (head != null) {
+            if (!isPalindromeCheck(head.next)) {
+                return false;
+            }
+            if (frontP.val != head.val) {
+                return false;
+            }
+            frontP = frontP.next;// 不要忘记
+        }
+        return true;
+    }
+
+    public boolean isPalindromeArr(ListNode head) {
+        List<Integer> cache = new ArrayList<>();
+        ListNode p = head;
+        while (p != null) {
+            cache.add(p.val);
+            p = p.next;
+        }
+        // check;
+        int left = 0;
+        int right = cache.size() - 1;
+        while (left < right) {
+            if (!cache.get(left).equals(cache.get(right))) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public boolean isPalindromeSB(ListNode head) {
         StringBuffer sb = new StringBuffer();
-        ListNode p  = head;
-        while(p!=null) {
+        ListNode p = head;
+        while (p != null) {
             sb.append(p.val);
             p = p.next;
         }
         // check;
         String str = sb.toString();
-        for(int i = 0; i< str.length() /2; i++) {
-            if(str.charAt(i) != str.charAt(str.length()  - i - 1)) {
+        for (int i = 0; i < str.length() / 2; i++) {
+            if (str.charAt(i) != str.charAt(str.length() - i - 1)) {
                 return false;
             }
         }
         return true;
     }
 }
+
 public class PalindromeLinkedList {
     public static void main(String[] args) {
         // 使用链式构造函数更简洁的写法：
