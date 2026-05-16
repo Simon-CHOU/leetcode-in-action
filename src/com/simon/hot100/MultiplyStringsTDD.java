734c0a5fd371b503fcb5d4e479ff6f5346d0bbaf
Simon-CHOU <mrsimonzhou@gmail.com>
2026-05-17 01:07:04 +0800

    43. 字符串相乘 gem-lee coach辅助下第一次成功
    https://leetcode.cn/problems/multiply-strings/
    
    https://gemini.google.com/gem/d9b9175d8ffc/0b47f76a2d97e476
    
    [0, 6]
    PASS exp=6 , act=6
    [0, 5, 6, 0, 8, 8]
    PASS exp=56088 , act=56088

--- src/com/simon/hot100/MultiplyStringsTDD.java
+++ src/com/simon/hot100/MultiplyStringsTDD.java
@@ -1,5 +1,6 @@
 package com.simon.hot100;
 
+import java.util.Arrays;
 import java.util.Objects;
 
 /**
@@ -9,23 +10,48 @@ import java.util.Objects;
 public class MultiplyStringsTDD {
     public String multiply(String num1, String num2) {
 
-        int i = num1.length() - 1;
-        int j = num2.length() - 1;
-        int carry = 0 ;
-        int sum = 0 ;
-        StringBuilder res = new StringBuilder();
-        while( i>=0 || j>=0 || carry >0) {
-            if(i>0) {sum * (res.charAt(i)--'0') * }
-            if(j>0) {res.charAt(j)--'0'}
+        int n1 = num1.length() ;
+        int n2 = num2.length() ;
+//        int carry = 0 ;
+//        int sum = 0 ;
+        int res[] = new int[num1.length()+num2.length()];
+        for (int i = n1 - 1; i >= 0; i--) {
+            for (int j = n2 - 1; j >= 0; j--) {
+                // 1. 纯粹的单步乘法，不要乘 10^k
+                int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+
+// 2. 算上低位格子（右边格子）里原有的累加值
+                int sum = mul + res[i + j + 1];
+
+// 3. 低位格子只留个位数
+                res[i + j + 1] = sum % 10;
+
+// 4. 高位格子（左边格子）直接「累加」进位
+// 注意：这里是 +=，因为高位格子可能在之前的循环里已经有数了
+                res[i + j] += sum / 10;
+            }
         }
-        return "0";
+        System.out.println(Arrays.toString(res));
+        // 消除前导零 res
+
+        StringBuilder clr = new StringBuilder();
+        boolean flag= true;
+        for(int i : res) {
+            //高位到低位
+            if(flag && i == 0 ) {
+                flag =false;
+            }else {
+                clr.append(i);
+            }
+        }
+        return clr.toString();
     }
 
     static void test(String exp, String act) {
         if(Objects.equals(exp, act)) {
-            System.out.println("PASS");
+            System.out.println("PASS exp="+exp+" , act="+act);
         } else {
-            System.out.println("FAIL");
+            System.out.println("FAIL exp="+exp+" , act="+act);
         }
     }
     public static void main(String[] args) {
